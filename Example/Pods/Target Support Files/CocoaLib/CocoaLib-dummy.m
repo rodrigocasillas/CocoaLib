0c3ae27a76a517982f38ba185f0cb7bc36641dc6
Rodrigo Casillas <rodrigocasillas@MacBook-Air-de-Rodrigo.local>
2019-07-12 13:28:31 -0500

    First library commit

---
+++ Example/Pods/Target Support Files/CocoaLib/CocoaLib-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_CocoaLib : NSObject
+@end
+@implementation PodsDummy_CocoaLib
+@end
---
+++ Example/Pods/Target Support Files/Pods-CocoaLib_Example/Pods-CocoaLib_Example-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_CocoaLib_Example : NSObject
+@end
+@implementation PodsDummy_Pods_CocoaLib_Example
+@end
